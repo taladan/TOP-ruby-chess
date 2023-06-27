4e17eaf2d0a3faaffc29022c7578b6a9f576bbcf
Jamie Crosby <taladan@gmail.com>
2023-06-27 11:27:28 -0400

    refactor: modify file structure for gem management

---
+++ lib/TOP/ruby/chess.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "chess/version"
+
+module TOP
+  module Ruby
+    module Chess
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/TOP/ruby/chess/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module TOP
+  module Ruby
+    module Chess
+      VERSION = "0.1.0"
+    end
+  end
+end
