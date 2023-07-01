e5d72d8e06e5278d939e671d416600bd6293745c
Jamie Crosby <taladan@gmail.com>
2023-07-01 09:53:06 -0400

    fix: typo: single to double quotes

--- lib/chess/display.rb
+++ lib/chess/display.rb
@@ -1,5 +1,5 @@
 # display.rb
-require 'colorize'
+require "colorize"
 
 # Handle building display for chessboard
 module Display
@@ -7,7 +7,7 @@ module Display
   # every square is treated as having a top, middle, and bottom pixel.  Contents of
   # each square will be displayed only in the middle pixel of the square.
   def build_row_string(row)
-    output = { top: '', middle: '', bottom: '' }
+    output = { top: "", middle: "", bottom: "" }
     row.each do |square|
       output[:top] << square.pixels[:top]
       output[:middle] << square.pixels[:middle]
@@ -19,13 +19,13 @@ module Display
 
   # Display handled by @display
   def update_display
-    # @rows - 1..0 because we're searching by position
+    # @rows - 1..0 because we"re searching by position
     # Build by row
     (@rows - 1).downto(0) { |row| puts build_row_string(build_row(row)) }
     linebreak
-    printf('     ')
+    printf("     ")
     column_labels.each do |label|
-      printf('%-6s', label)
+      printf("%-6s", label)
     end
     linebreak
   end
@@ -35,19 +35,19 @@ module Display
   # insert newline
   # Add row number to head of row in middle pixel
   def add_row_header(row, output)
-    output[:top].insert(0, '   ')
+    output[:top].insert(0, "   ")
     output[:middle].insert(0, " #{row[0].position[1] + 1} ")
-    output[:bottom].insert(0, '   ')
+    output[:bottom].insert(0, "   ")
     output
   end
 
   def linebreak
-    puts ' '
+    puts " "
   end
 
   # set color of string to background of square
   def set_color(string, color)
-    if color == 'black'
+    if color == "black"
       string.on_black
     else
       string.on_white
