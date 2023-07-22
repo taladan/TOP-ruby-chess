3ca5541acd60043e0554372be1a915cb7b46c4c6
Jamie Crosby <taladan@gmail.com>
2023-07-22 19:51:49 -0400

    feat: add messaging used by Display

--- lib/chess/messages.rb
+++ lib/chess/messages.rb
@@ -13,6 +13,9 @@ module Display
         piece: "#{prompt} select piece (ex: a2) #{prompt_terminator}",
         target: "#{prompt} select target square (ex: a4) #{prompt_terminator}",
         invalid_square: "#{prompt} that is not a valid target.",
+        invalid_piece: "#{prompt} that is not a valid piece.",
+        player_one_prompt: "#{prompt} enter the name of player 1",
+        player_two_prompt: "#{prompt} enter the name of player 2",
       }
     end
 
