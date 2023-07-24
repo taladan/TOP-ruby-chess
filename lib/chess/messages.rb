13b88bddc0015e7168ab6bd32cd24c02ed628e30
Jamie Crosby <taladan@gmail.com>
2023-07-23 22:02:23 -0400

    feat: add prompt and messages

--- lib/chess/messages.rb
+++ lib/chess/messages.rb
@@ -6,16 +6,18 @@ module Display
   class Messages
     attr_reader :messages
 
-    def initialize(player)
-      prompt = "Chess: #{player.name},"
+    def initialize(player=nil)
+      prompt = "Chess: " unless player
+      prompt = "Chess: #{player.name}," if player
       prompt_terminator = "> "
       @messages = {
         piece: "#{prompt} select piece (ex: a2) #{prompt_terminator}",
         target: "#{prompt} select target square (ex: a4) #{prompt_terminator}",
         invalid_square: "#{prompt} that is not a valid target.",
         invalid_piece: "#{prompt} that is not a valid piece.",
-        player_one_prompt: "#{prompt} enter the name of player 1",
-        player_two_prompt: "#{prompt} enter the name of player 2",
+        player_two_prompt: "#{prompt} enter the name of player 2 #{prompt_terminator}",
+        player_one_prompt: "#{prompt} enter the name of player 1 #{prompt_terminator}",
+        game_type_prompt: "#{prompt} enter game type, (S)tandard or (C)ustom #{prompt_terminator}",
       }
     end
 
