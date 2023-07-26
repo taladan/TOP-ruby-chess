8c03a7c653c29de8e5146da8ebdeca08da83cc2b
Jamie Crosby <taladan@gmail.com>
2023-07-26 13:26:59 -0400

    refactor: change how possible moves are handled

--- lib/chess/pieces/pawn.rb
+++ lib/chess/pieces/pawn.rb
@@ -12,15 +12,22 @@ module Pieces
       @icon = "♟"
       @has_moved = false
       super(piece, color, square)
+      @possible_moves = possibles
     end
 
     # Pawn can move 2 squares forward in first move or 1 square forward first move, 
     # subsequent moves are 1 square forward.  May only attack on a forward diagonal.  
     # Can't move backwards at all.
     # Range of posslble movements
-    def self.possible_moves(color)
-      return [[0, 1], [0, 2], [1, 1], [-1, 1]] if color == "white"
-      return [[0, -1], [0, -2], [1, -1], [-1, -1]] if color == "black"
+    def possibles
+      # hasn't moved, white
+      return [[0, 1], [0, 2], [1, 1], [-1, 1]] if @color == "white" && @has_moved == false
+      # hasn't moved, black
+      return [[0, -1], [0, -2], [1, -1], [-1, -1]] if @color == "black" && @has_moved == false
+      # has moved, white
+      return [[0, 1], [1, 1], [-1, 1]] if @color == "white" && @has_moved
+      # has moved, black
+      return [[0, -1], [1, -1], [-1, -1]] if @color == "black" && @has_moved
     end
   end
 end
