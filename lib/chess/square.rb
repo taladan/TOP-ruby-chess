566f921bc18ddfebe6c8fcfd088c098ae094f684
Jamie Crosby <taladan@gmail.com>
2023-07-31 10:17:00 -0400

    clean: remove unused code & clean comments

--- lib/chess/square.rb
+++ lib/chess/square.rb
@@ -3,7 +3,7 @@
 
 require "colorize"
 
-# Creates a graph node named 'square'
+# a graph node named 'square'
 class Square
   attr_reader :x, :y
   attr_accessor :name,
@@ -29,7 +29,7 @@ class Square
     @prev = nil
   end
 
-  # Assign square color based on position within board.  Upper left & Lower right squares == whitejj
+  # assign square color based on position within board.  Upper left & Lower right squares == whitejj
   def assign_color
     if (@position[0].even? && @position[1].even?) || (@position[0].odd? && @position[1].odd?)
       @color = "black"
@@ -38,17 +38,12 @@ class Square
     end
   end
 
-  # Return true/false if square at beginning of row
-  def head_of_row?
-    position[1].zero?
-  end
-
-  # Return true/false if square occupied
+  # return true/false if square occupied
   def occupied?
     !@contents.nil?
   end
 
-  # Build square display
+  # build square display
   def pixels
     pad = "      ".on_black if @color == "black"
     pad = "      ".on_white if @color == "white"
@@ -66,6 +61,7 @@ class Square
     { top: pad, middle: mid, bottom: pad }
   end
 
+  # assigns X and Y coordinates of square from passed 2 value array
   def assign_positions(arr)
     @position = arr
     @x = @position[0]
