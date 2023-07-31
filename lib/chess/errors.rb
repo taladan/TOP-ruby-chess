38889da2decdb1e7d08f2e15ea88384ccb59baa8
Jamie Crosby <taladan@gmail.com>
2023-07-30 21:48:48 -0400

    docs: update error documentation

--- lib/chess/errors.rb
+++ lib/chess/errors.rb
@@ -1,61 +1,67 @@
 # lib/errors.rb
 # frozen_string_literal: true
 
+# Plan to move forward:
+# - create a directory for errors
+# - break all errors out into their own file
+# - figure out how to aggregate all error files in that directory into `errors.rb`
+
 # Namespace for all chess errors
 module ChessErrors
-  # Base chess error
+  # base chess error
   class ChessError < StandardError
   end
 
-  # An invalid Target square has been given
+  # an invalid Target square has been given
   class InvalidTargetPositionError < ChessError
     def initialize(msg = "Target square given is not a valid square.")
       super
     end
   end
 
-  # An invalid piece name has been given
+  # an invalid piece name has been given
   class InvalidPieceNameError < ChessError
     def initialize(msg = "Invalid piece name given.")
       super
     end
   end
 
-  # An invalid starting position has been given
+  # an invalid starting position has been given
   class InvalidStartingPositionError < ChessError
     def initialize(msg = "Invalid Starting Position.")
       super
     end
   end
 
-  # An empty square has been found when a chess piece was expected
+  # an empty square has been found when a chess piece was expected
   class EmptySquareError < ChessError
     def initialize(msg = "No piece in starting square.")
       super
     end
   end
 
-  # An illegal piece move has been given
+  # an illegal piece move has been given
   class IllegalMoveError < ChessError
     def initialize(msg = "Invalid move for indicated piece.")
       super
     end
   end
 
-  # Invalid input given by user
+  # invalid input given by user
   class InvalidInputError < ChessError
     def initialize(msg = "Invalid input given.")
       super
     end
   end
 
-  # Player tried moving opponent's piece
+  # player tried moving opponent's piece
   class OpponentsPieceChosenError < ChessError
     def initialize(msg = "You cannot move an opponent's piece.")
       super
     end
   end
-  
+
+  # path is not clear
   class PathError < ChessError
     def initialize(msg = "There is a piece in the way.")
       super
